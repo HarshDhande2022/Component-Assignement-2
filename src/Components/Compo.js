8b94db94c80891febb0bb9e023259837c1c86bba
HarshDhande2022 <98374763+HarshDhande2022@users.noreply.github.com>
2022-11-04 15:01:16 +0530

    Add files via upload

---
+++ src/Components/Compo.js
@@ -0,0 +1,49 @@
+import React from "react";
+export default function Compo() {
+  return (
+    <div>
+      <div>
+        <h3>Harsh Dhande</h3>
+        <p>Software Developer,Educator at Geekster </p>
+        <hr />
+      </div>
+      <div>
+        <h4>Experience</h4>
+        <p>Software Developer at Geekster</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo vitae vel
+          quae illum fuga id culpa aliquid consectetur corrupti. Ullam
+          doloremque aperiam maiores iste perferendis laborum corrupti pariatur
+          reprehenderit numquam illum! Sunt unde hic dignissimos reprehenderit
+          molestias fugit natus nostrum!
+        </p>
+        <hr />
+        <p>Software Developer</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo vitae vel
+          quae illum fuga id culpa aliquid consectetur corrupti. Ullam
+          doloremque aperiam maiores iste perferendis laborum corrupti pariatur
+          reprehenderit numquam illum! Sunt unde hic dignissimos reprehenderit
+          molestias fugit natus nostrum!
+        </p>
+        <hr />
+        <h2>Skills</h2>
+        <ul>
+          <li>Java</li>
+          <li>JavaScript</li>
+          <li>My-Sql</li>
+          <li>React JS</li>
+          <li>Node JS</li>
+        </ul>
+        <hr />
+        <h2>Hobbies</h2>
+        <ol>
+          <li>FootBall</li>
+          <li>Cricket</li>
+          <li>Swiming</li>
+          <li>Reading</li>
+        </ol>
+      </div>
+    </div>
+  );
+}
